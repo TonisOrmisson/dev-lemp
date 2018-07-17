543fed47b9d05bcfa8cdead077afbf3e8f4b582d
Tõnis Ormisson <tonis@andmemasin.eu>
2018-07-17 10:06:50 +0300

    bump version

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 docker build --tag tonisormisson/dev-lemp .
 docker tag tonisormisson/dev-lemp:latest tonisormisson/dev-lemp:0.3.5
-docker push tonisormisson/dev-lemp:latest && docker push tonisormisson/dev-lemp:0.3.4
+docker push tonisormisson/dev-lemp:latest && docker push tonisormisson/dev-lemp:0.3.5
