02d9dcd0d5fac21a71badb88341698e001b40699
Tonis Ormisson <tonis@andmemasin.eu>
2019-06-28 11:43:17 +0300

    do not exit script if service starting fails (could already be running)

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 docker build --tag tonisormisson/dev-lemp .
-docker tag tonisormisson/dev-lemp:latest tonisormisson/dev-lemp:0.5.5
-docker push tonisormisson/dev-lemp:0.5.5 && docker push tonisormisson/dev-lemp:latest
+docker tag tonisormisson/dev-lemp:latest tonisormisson/dev-lemp:0.6.0
+docker push tonisormisson/dev-lemp:0.6.0 && docker push tonisormisson/dev-lemp:latest
--- start.sh
+++ start.sh
@@ -3,37 +3,16 @@
 # Start mysql
 echo "Starting mysql "
 find /var/lib/mysql -type f -exec touch {} \; && service mysql start
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start mysql: $status"
-  exit $status
-fi
-
 
 # Start PHP-fpm
 echo "Starting PHP-fpm "
 service php7.2-fpm start
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start PHP-fpm: $status"
-  exit $status
-fi
 
 # Start selenium
 echo "Starting Selenium ... "
 export MOZ_HEADLESS=1 && java -jar selenium-server-standalone-*.jar -enablePassThrough false  > /var/log/selenium.log 2> /var/log/selenium_error.log &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start Selenium: $status"
-  exit $status
-fi
 
 # Start nginx
-echo "Starting nginx .... "
+echo "Starting nginx ... "
 nginx -g "daemon off;"
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start nginx: $status"
-  exit $status
-fi
 
